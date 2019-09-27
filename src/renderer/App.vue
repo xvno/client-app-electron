b83f3a4b8292d40f1cc48eda3f195222f921c130
volving <volving@icloud.com>
2019-09-27 19:56:26 +0800

    init

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'client-app-electron',
+  };
+</script>
+
+<style>
+  /* CSS */
+</style>
